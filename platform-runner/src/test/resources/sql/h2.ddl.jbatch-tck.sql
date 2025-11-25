e7221e880ea60720128318b2fd2ff0a44fd9ca75
James R. Perkins <jperkins@ibm.com>
2025-11-24 20:28:15 -0800

    Change the TCK runner to use maven for a single project to run and a more simple approach.
    Signed-off-by: James R. Perkins <jperkins@ibm.com>

---
+++ platform-runner/src/test/resources/sql/h2.ddl.jbatch-tck.sql
@@ -0,0 +1,88 @@
+CREATE SCHEMA IF NOT EXISTS APP;
+SET SCHEMA APP;
+
+DROP TABLE IF EXISTS Numbers;
+DROP TABLE IF EXISTS  Orders;
+DROP TABLE IF EXISTS  Inventory;
+
+CREATE TABLE Numbers
+(
+    item     INT,
+    quantity INT
+);
+
+CREATE TABLE Orders
+(
+    orderID  IDENTITY(1,1) PRIMARY KEY NOT NULL,
+    itemID   INT,
+    quantity INT
+);
+
+CREATE TABLE Inventory
+(
+    itemID   INT NOT NULL PRIMARY KEY,
+    quantity INT NOT NULL
+);
+
+INSERT INTO Inventory
+VALUES (1, 100);
+
+INSERT INTO Numbers
+VALUES (1, 10);
+
+INSERT INTO Numbers
+VALUES (2, 10);
+
+INSERT INTO Numbers
+VALUES (3, 10);
+
+INSERT INTO Numbers
+VALUES (4, 10);
+
+INSERT INTO Numbers
+VALUES (5, 10);
+
+INSERT INTO Numbers
+VALUES (6, 10);
+
+INSERT INTO Numbers
+VALUES (7, 10);
+
+INSERT INTO Numbers
+VALUES (8, 10);
+
+INSERT INTO Numbers
+VALUES (9, 10);
+
+INSERT INTO Numbers
+VALUES (10, 10);
+
+INSERT INTO Numbers
+VALUES (11, 10);
+
+INSERT INTO Numbers
+VALUES (12, 10);
+
+INSERT INTO Numbers
+VALUES (13, 10);
+
+INSERT INTO Numbers
+VALUES (14, 10);
+
+INSERT INTO Numbers
+VALUES (15, 10);
+
+INSERT INTO Numbers
+VALUES (16, 10);
+
+INSERT INTO Numbers
+VALUES (17, 10);
+
+INSERT INTO Numbers
+VALUES (18, 10);
+
+INSERT INTO Numbers
+VALUES (19, 10);
+
+INSERT INTO Numbers
+VALUES (20, 10);
